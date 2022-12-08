f2315afe7ae8504b3c2c473fa0a6d3d0ea9be856
amita <amitasher4@gmail.com>
2022-12-08 15:09:10 +0200

    some method project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+/// components
+import Movies from './components/Movies';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Movies.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+/// import file movies css
+import "../css/Movies.css";
+
+function Movies() {
+  const [movies, setMovies] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%40", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "x-rapidapi-key": "9539b5ed1fmsh166ec60437e5d9ep127e4fjsnf0c44d725c91",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setMovies(data.d))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  console.log(movies);
+
+  return (
+    <div className="sectionDiv">
+      <div className="inputDivAround">
+        <input
+          className="SearchInput"
+          placeholder="type the of movie"
+          onChange={(event) => {
+            setInputText(event.target.value);
+          }}
+        />
+      </div>
+
+      <div className="disgnMainDivMovies">
+        {movies.length ? (
+          movies
+            .filter((movie) => {
+              if (movie === "") {
+                return movie;
+              } else if (
+                movie.l.toLowerCase().includes(inputText.toLowerCase())
+              ) {
+                return movie;
+              }
+            })
+            .map((movie) => {
+              return (
+                <div className="mainMovies" key={movie.id}>
+                  <img src={movie.i.imageUrl} alt="meshiImg" />
+                  <p className="nameOfMovie">{movie.l}</p>
+                  <p className="yearOfMovie">{movie.y}</p>
+                </div>
+              );
+            })
+        ) : (
+          <h1>loading</h1>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Movies;
